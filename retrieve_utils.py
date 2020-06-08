a5923213fa18ac06ad40f7cd68c24fe306d0feb7
selina khan <selinajasmin@gmail.com>
2020-06-08 14:26:03 +0200

    get_frames

--- retrieve_utils.py
+++ retrieve_utils.py
@@ -1,5 +1,18 @@
+import imageio
+
 def get_body_part_bboxes(video_path):
     pass
 
 def get_ball_bboxes(video_path):
     pass
+
+def get_frames(video_path):
+    video = imageio.get_reader(video_path)
+    no_frames = video.count_frames()
+
+    frames = []
+
+    for i in range(no_frames):
+        im = video.get_data(i)
+        frames.append(im)
+    return frames
