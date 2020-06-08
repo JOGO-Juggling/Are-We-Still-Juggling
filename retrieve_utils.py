9305b92e94bd0cc31f262bb2811b72184d80fa48
selina khan <selinajasmin@gmail.com>
2020-06-08 14:28:36 +0200

    variable change

--- retrieve_utils.py
+++ retrieve_utils.py
@@ -8,11 +8,11 @@ def get_ball_bboxes(video_path):
 
 def get_frames(video_path):
     video = imageio.get_reader(video_path)
-    no_frames = video.count_frames()
+    total_frames = video.count_frames()
 
     frames = []
 
-    for i in range(no_frames):
+    for i in range(total_frames):
         im = video.get_data(i)
         frames.append(im)
     return frames
