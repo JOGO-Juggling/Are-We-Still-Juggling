ffa71846f19ee326e2078f377bf199d7fe367210
Sam Titarsolej <s.titarsolej@gmail.com>
2020-06-10 11:49:51 +0200

    test functionality in main loop

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-from bounce import is_bounce
+from retrieve_utils import BodyReader, VideoReader
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as patches
 
-if __name__ == '__main__':
-    pass
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+videoreader = VideoReader('data/videos/0f7aa7cb9bf54f8ca89d551ecdccc735.MOV')
+bodyreader = BodyReader('data/keypoints.json', '0f7aa7cb9bf54f8ca89d551ecdccc735.MOV')
+frames = []
+
+for i, (frame, keypoints) in enumerate(zip(videoreader, bodyreader)):
+    result = [ax.imshow(frame, animated=True)]
+    if keypoints != {}:
+        if keypoints['RAnkle'] != {}:
+            x, y = int(keypoints['RAnkle']['x']), int(keypoints['RAnkle']['y'])
+            circle = patches.Circle((x, y), 20, color='r')
+            result.append(ax.add_patch(circle))
+        if keypoints['LAnkle'] != {}:
+            x, y = int(keypoints['LAnkle']['x']), int(keypoints['LAnkle']['y'])
+            circle = patches.Circle((x, y), 20, color='g')
+            result.append(ax.add_patch(circle))
+    frames.append(result)
+
+animation = animation.ArtistAnimation(fig, frames, interval=32)
+animation.save('result.mp4')
+# plt.show()
