19ef2dd77ed87424f836f31c2c339f2aea98c2d1
Sam Titarsolej <s.titarsolej@gmail.com>
2020-06-10 12:26:02 +0200

    ball trajectory

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from retrieve_utils import BodyReader, VideoReader
+from retrieve_utils import BodyReader, VideoReader, BallReader
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.patches as patches
@@ -6,21 +6,28 @@ import matplotlib.patches as patches
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-videoreader = VideoReader('data/videos/0f7aa7cb9bf54f8ca89d551ecdccc735.MOV')
-bodyreader = BodyReader('data/keypoints.json', '0f7aa7cb9bf54f8ca89d551ecdccc735.MOV')
+video = '0f272d8bfd2947a0b37f0deaf2fe20bd.MOV'
+videoreader = VideoReader(f'data/videos/{video}')
+bodyreader = BodyReader('data/keypoints.json', video)
+ballreader = BallReader('data/balls.json', video)
 frames = []
 
-for i, (frame, keypoints) in enumerate(zip(videoreader, bodyreader)):
+for i, (frame, keypoints, ball) in enumerate(zip(videoreader, bodyreader, ballreader)):
     result = [ax.imshow(frame, animated=True)]
     if keypoints != {}:
         if keypoints['RAnkle'] != {}:
             x, y = int(keypoints['RAnkle']['x']), int(keypoints['RAnkle']['y'])
-            circle = patches.Circle((x, y), 20, color='r')
+            circle = patches.Circle((x, y), 10, color='r')
             result.append(ax.add_patch(circle))
         if keypoints['LAnkle'] != {}:
             x, y = int(keypoints['LAnkle']['x']), int(keypoints['LAnkle']['y'])
-            circle = patches.Circle((x, y), 20, color='g')
+            circle = patches.Circle((x, y), 10, color='g')
             result.append(ax.add_patch(circle))
+    if ball != {}:
+        x, y = int(ball['x']), int(ball['y'])
+        w, h = int(ball['width']), int(ball['height'])
+        square = patches.Rectangle((x, y), w, h, )
+        result.append(ax.add_patch(square))
     frames.append(result)
 
 animation = animation.ArtistAnimation(fig, frames, interval=32)
