73d3779a5b526f99c68712ec8c2a6326ef55ecb2
selina khan <selinajasmin@gmail.com>
2020-06-23 13:43:57 +0200

    comments

--- retrieve_utils.py
+++ retrieve_utils.py
@@ -4,8 +4,9 @@ import numpy as np
 import cv2
 
 class BodyReader:
+    ''' Load keypoint data of body parts per frame and process them for easy use. ''' 
     def __init__(self, data_path, video_name):
-        ''' Retrieve data for specific video and determine the amount of frames '''
+        # Initialize video by name and data file
         self.data_path = data_path 
         self.video_name = ''.join(video_name.split('.')[:-1])
 
@@ -15,10 +16,12 @@ class BodyReader:
         self.max_frame = int(len(self.data) - 1)
 
     def __iter__(self):
+        # Create iterator
         self.cur_frame = 0
         return self
 
     def __next__(self):
+        # Iterate over frames and retrieve keypoint data if present
         if self.cur_frame < self.max_frame:
             if self.data[str(self.cur_frame)]['LAnkle']['x'] != -1 and self.data[str(self.cur_frame)]['RAnkle']['x'] != -1:
                 result = self.data[str(self.cur_frame)]
@@ -36,7 +39,9 @@ class BodyReader:
             raise StopIteration
 
 class BallReader:
+    ''' Load ball data per frame and process them for easy use. ''' 
     def __init__(self, data_path, video_name):
+        # Initialize video by name and data file
         self.data_path = data_path
         self.video_name = video_name
 
@@ -46,10 +51,12 @@ class BallReader:
         self.max_frame = int(len(self.data) - 1)
         
     def __iter__(self):
+        # Create iterator
         self.cur_frame = 0
         return self
 
     def __next__(self):
+        # Iterate over frames and retrieve location of the ball if present
         if self.cur_frame < self.max_frame:
             if len(self.data[str(self.cur_frame)]) > 0:
                 del(self.data[str(self.cur_frame)][0]['confidence'])
@@ -62,8 +69,8 @@ class BallReader:
             raise StopIteration
 
 class VideoReader:
+    ''' Retrieve data for specific video and determine the amount of frames '''
     def __init__(self, video_path):
-
         self.video = cv2.VideoCapture(video_path)
         self.total_frames = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
         width = self.video.get(cv2.CAP_PROP_FRAME_WIDTH)
